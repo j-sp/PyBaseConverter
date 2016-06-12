21908317e85a1a0bdea705db30589547874fb1ac
Javier Serrano <Javier.Serrano.Pareja@gmail.com>
2016-06-12 14:58:16 +0200

    Add docstrings.

--- Number.py
+++ Number.py
@@ -15,15 +15,42 @@
 ## You should have received a copy of the GNU General Public License
 ## along with PyBaseConverter.  If not, see <http://www.gnu.org/licenses/>.
 
+"""A Number class one can get and set using different bases.
+
+This module implements class Number, a place holder for a number
+one can set and get using different bases.
+"""
+
 class Number:
-    
+    """A class for holding a number one can get and set using different bases.
+
+    Public methods:
+    __init__: initialize with a value in base 10.
+    __str__ : provide string representation for number in base 10.
+    set     : set the number using a number string and a given base.
+    get     : get the number in a given base.
+    """
     def __init__(self, value):
+        """ Initialize number with a value in base 10."""
         self.base10_value = value
 
     def __str__(self):
+        """ Return string representation of number in base 10."""
         return str(self.base10_value)
     
     def set(self, base, number_string):
+        """Set value of number using a number string in a given base.
+        
+        Args:
+          base: a number between 2 and 10 included.
+          number_string: a string representing the number in the given base.
+
+        Exceptions raised:
+          ValueError if the base is not comprised between 2 and 10.
+          ValueError if one of the characters in the string does not correspond
+          to an integer (0-9).
+          ValueError if at least one of the digits is not smaller than the base.
+        """
         if base > 10 or base < 2:
             raise ValueError("base should be comprised between 2 and 10")
         accum = 0
@@ -40,6 +67,17 @@ class Number:
         self.base10_value = accum
 
     def get(self, base):
+        """Get number in a given base.
+
+        Args:
+          base: a number specifying the base.
+
+        Returns:
+          A string containing the representation of the number in the given base.
+
+        Exceptions raised:
+          ValueError if the base is not comprised between 2 and 10 included.
+        """
         if base > 10 or base < 2:
             raise ValueError("base should be comprised between 2 and 10")
         result = ""
