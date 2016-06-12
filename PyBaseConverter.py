10409da7c986af9954c077b7958b73d99ed9dac1
Javier Serrano <Javier.Serrano.Pareja@gmail.com>
2016-06-12 21:07:43 +0200

    Complete docstrings

--- PyBaseConverter.py
+++ PyBaseConverter.py
@@ -28,7 +28,8 @@ class Form(QWidget):
     """ Class derived from QWidget to hold our top-level window.
 
     Public methods:
-      To be filled
+      __init__     : initialize Form
+      submitNumbers: calculate number in new base 
     """
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
