21c0c1719896928f615126db9a11d7a97d786ebc
Javier Serrano <Javier.Serrano.Pareja@gmail.com>
2016-06-14 21:59:09 +0200

    Stop importing all of QtWidgets

--- PyBaseConverter.py
+++ PyBaseConverter.py
@@ -20,8 +20,9 @@
 This code can only convert from/to bases from 2-10.
 """
 
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QApplication, QWidget, \
+                            QLabel, QLineEdit, QPushButton, \
+                            QVBoxLayout, QGridLayout
 import Number
 
 class Form(QWidget):
